75d8e37560582aaa01e5b3a634487e76c4e22f5f
karthick.d <karthick.d@infinitisoftware.net>
2025-07-28 12:58:34 +0530

    Build file changes for micre front end concepts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: "./src/Exported.tsx",
+      name: "mailerEngine",
+      fileName: "infi-chatbot",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   css: {
     preprocessorOptions: {
