1e5084e56259258a76fc68ee27de92bf263a5f4c
karthick.d <karthick.d@infinitisoftware.net>
2025-07-30 17:05:16 +0530

    Updated changes for microfront build

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.scss';
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App.tsx';
+// import './index.scss';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reactToWebComponent from 'react-to-webcomponent';
+
+const WebComponent = reactToWebComponent(App, React, ReactDOM);
+customElements.define('react-mf', WebComponent);
--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,22 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
   build: {
     lib: {
-      entry: "./src/Exported.tsx",
-      name: "mailerEngine",
-      fileName: "infi-chatbot",
-      formats: ["es"],
+      entry: 'src/main.tsx',
+      name: 'ReactMF',
+      fileName: () => 'chatBot.js',
+      formats: ['umd'],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
